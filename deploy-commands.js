d44f6d448ee894e71233a95f5890e215b8eb1a63
Gregory Michaels <kicktools@outlook.com>
2025-02-13 10:50:07 -0500

    Updated App Guild Commands to include Guild

--- deploy-commands.js
+++ deploy-commands.js
@@ -23,7 +23,7 @@ const rest = new REST().setToken(process.env.DISCORD_TOKEN);
         console.log('Started refreshing application (/) commands.');
 
         await rest.put(
-            Routes.applicationCommands(process.env.CLIENT_ID),
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands },
         );
 
